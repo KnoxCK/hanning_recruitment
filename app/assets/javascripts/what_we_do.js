5b70ebc26f8a5d154c071e80daa5636fb19867c0
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-02 11:25:04 +0100

    what we do page working sexy

--- app/assets/javascripts/what_we_do.js
+++ app/assets/javascripts/what_we_do.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
     $(".sector-circle img").removeClass("active");
     $(this).children("img").addClass('active');
     $(".sector-individual").removeClass("active");
-    $($(this).data("target").addClass("active"));
+    $($(this).data("target")).addClass("active");
     $('html,body').animate({
       scrollTop: $(".sector-more-info").offset().top - 100},
-      2000);
+      1000);
   });
 });
