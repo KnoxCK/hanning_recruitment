e3ded65178b0ef5100db31ecc6445d47780f49c0
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-03 16:01:20 +0100

    create enquiries method

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,4 +1,12 @@
 class EnquiriesController < ApplicationController
   def create
+    @enquiry = Enquiry.create(enquiry_params)
+    redirect_to contact_path
+  end
+
+  private
+
+  def enquiry_params
+    params.require(:enquiry).permit(:name, :email, :tel, :body)
   end
 end
