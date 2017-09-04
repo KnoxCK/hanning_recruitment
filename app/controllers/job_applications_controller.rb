0c7943456acd3822b8b5e2d4d6711af64c0808d9
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-04 20:23:32 +0100

    form and role link working sexy

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,6 +1,7 @@
 class JobApplicationsController < ApplicationController
   def create
     @job_application = JobApplication.new(job_application_params)
+    @job_application.live_role_id = params[:role_id]
     if @job_application.save
       redirect_to root_path
     end
