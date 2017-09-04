f98093aa266839de7d9f1ba4eed16fd5d763a744
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-04 10:07:29 +0100

    live roles js working sexy

--- app/assets/javascripts/live_roles.js
+++ app/assets/javascripts/live_roles.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
   $(".job-role").click(function() {
     $(".live-roles-more-info").removeClass("active");
     $($(this).data("target")).addClass("active");
+    $('html,body').animate({
+      scrollTop: $(".live-roles-more-info-wrapper").offset().top - 100},
+      1000);
   })
 })
