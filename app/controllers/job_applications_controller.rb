107cc407a9b24caf26edf142457ea22358ebf110
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-04 22:08:28 +0100

    JA

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -4,11 +4,9 @@ class JobApplicationsController < ApplicationController
     @job_application = JobApplication.new(job_application_params)
     @job_application.live_role_id = params[:role_id] if params[:role_id].present?
     if @job_application.save
-      flash[:notice] = "Your application has been sent"
+      flash[:notice] = "Thank you, your application has been sent. We will be in touch"
       JobApplicationMailer.job_application(@job_application).deliver_now
       redirect_to live_roles_path
-    else
-      redirect_to :back
     end
   end
 
