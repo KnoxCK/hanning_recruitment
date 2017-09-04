77be8604e3d7ea4467bf205dca7e5260ceeff399
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-04 22:03:37 +0100

    app form on navbar

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,11 +1,14 @@
 class JobApplicationsController < ApplicationController
   def create
+    # binding.pry
     @job_application = JobApplication.new(job_application_params)
-    @job_application.live_role_id = params[:role_id]
+    @job_application.live_role_id = params[:role_id] if params[:role_id].present?
     if @job_application.save
       flash[:notice] = "Your application has been sent"
       JobApplicationMailer.job_application(@job_application).deliver_now
       redirect_to live_roles_path
+    else
+      redirect_to :back
     end
   end
 
