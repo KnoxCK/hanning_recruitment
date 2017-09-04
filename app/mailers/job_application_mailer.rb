3692b6edf2eb74aaf9a3abd64a433d9cec0be475
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-04 21:11:21 +0100

    JA mail

--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -1,2 +1,8 @@
 class JobApplicationMailer < ApplicationMailer
+  def job_application(job_application)
+    @job_application = job_application
+
+    mail(to: "info@hanningrecruitment.com",
+          subject: "You have received an application")
+  end
 end
