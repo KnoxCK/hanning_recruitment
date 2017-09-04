ed836ab79f77b371a547bcd4f0bf65e73629a3f1
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-04 21:06:16 +0100

    JA mailer added

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -3,7 +3,9 @@ class JobApplicationsController < ApplicationController
     @job_application = JobApplication.new(job_application_params)
     @job_application.live_role_id = params[:role_id]
     if @job_application.save
-      redirect_to root_path
+      flash[:notice] = "Your application has been sent"
+      # JobApplicationMailer.job_application(@job_application).deliver_now
+      redirect_to live_roles_path
     end
   end
 
---
+++ app/mailers/job_application_mailer.rb
@@ -0,0 +1,2 @@
+class JobApplicationMailer < ApplicationMailer
+end
