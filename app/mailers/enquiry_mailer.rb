0265b034828e7be7c24162dff5aab18dd72c66e8
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-05 21:16:30 +0100

    email address and registration logic

--- app/mailers/enquiry_mailer.rb
+++ app/mailers/enquiry_mailer.rb
@@ -2,7 +2,7 @@ class EnquiryMailer < ApplicationMailer
   def enquiry_received(enquiry)
     @enquiry = enquiry
 
-    mail(to: "govinder.marwaha@gmail.com",
+    mail(to: "info@hanningrecruitment.com",
           subject: "You have received an enquiry")
   end
 end
