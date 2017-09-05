65bb93ae8bbbc09d88649eb73b30078446feeccf
Clarissa Knox <knox.ck@gmail.com>
2017-09-05 08:47:02 +0100

    footer

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 	setTimeout(function() {
     $(".banner-content").addClass("hidden");
     $(".landing-transparent").addClass("hidden");
+    $(".faded-logo").removeClass("hidden");
     $(".menu-circles").removeClass("hidden");
     $(".home-strip").removeClass("hidden");
     $(".landing-hanning").removeClass("hidden");
