d7cec060a5d791e15564a2358498903294f6c188
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-06 20:10:17 +0100

    registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < ApplicationController
+  def create
+    @registration = JobApplication.new(registration_params)
+    if @registration.save
+      flash[:notice] = "Thank you for registering with us. We will be in touch"
+      # JobApplicationMailer.registration(@registration).deliver_now
+      redirect_to live_roles_path
+    else
+      flash[:alert] = "You must upload a CV"
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def registration_params
+    params.require(:registration).permit(:name, :email, :phone, :cv)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'registrations/create'
+
   get 'job_applications/create'
 
   get 'enquiries/create'
