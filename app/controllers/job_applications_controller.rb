24987cbc085be2af7ce946919cf490c72407254d
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-06 20:11:16 +0100

    job app controller update

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -4,7 +4,7 @@ class JobApplicationsController < ApplicationController
   def create
     # binding.pry
     @job_application = JobApplication.new(job_application_params)
-    @job_application.live_role_id = params[:role_id] if params[:role_id].present?
+    @job_application.live_role_id = params[:role_id]
     if @job_application.save
       flash[:notice] = "Thank you, your application has been sent. We will be in touch"
       JobApplicationMailer.job_application(@job_application).deliver_now
