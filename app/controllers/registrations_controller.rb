bc7bb871cb9ad5859758ed1f3efe5c5edff3ca2d
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-06 21:08:00 +0100

    registrations working sexy

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < ApplicationController
   def create
-    @registration = JobApplication.new(registration_params)
+    @registration = Registration.new(registration_params)
     if @registration.save
       flash[:notice] = "Thank you for registering with us. We will be in touch"
       # JobApplicationMailer.registration(@registration).deliver_now
-      redirect_to live_roles_path
+      redirect_to root_path
     else
       flash[:alert] = "You must upload a CV"
       redirect_to :back
