5293c6671f17c8d2dc99b87f7c5022f1e823285e
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-14 14:51:53 +0100

    formatted date

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,8 @@
 class News < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+
+
+  def formatted_date
+    "#{self.date.strftime("%-d").to_i.ordinalize} #{self.date.strftime("%B %Y")}"
+  end
 end
