999bdae3044d28f85911c296ce3068a908081516
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-14 19:06:18 +0100

    images working sexy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :what_we_do, :about_us, :live_roles, :news, :news_article, :news_article_two, :contact]
-
+  before_action :instagram_call, only: :news
   def home
   end
 
@@ -19,8 +19,6 @@ class PagesController < ApplicationController
 
   def news
     @news = News.all.order(:created_at).reverse
-    url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=5855364746.49cacea.fa77e9e21f3b4d88b0e7347358e797e0"
-    @insta = JSON.parse(open(url).read)["data"][0]["link"]
   end
 
   def news_article
@@ -31,4 +29,16 @@ class PagesController < ApplicationController
     @enquiry = Enquiry.new
     @message = params[:message]
   end
+
+  private
+
+  def instagram_call
+    url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=5855364746.49cacea.fa77e9e21f3b4d88b0e7347358e797e0"
+    media = JSON.parse(open(url).read)["data"]
+    @image_urls = []
+    media.each do |media|
+      @image_urls << media["link"]
+    end
+  end
+
 end
