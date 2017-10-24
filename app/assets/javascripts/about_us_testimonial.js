cf28d63ce388ff4943ae5efa79c8c3e0239e6b81
Clarissa Knox <knox.ck@gmail.com>
2017-10-24 17:10:12 +0100

    about

--- app/assets/javascripts/about_us_testimonial.js
+++ app/assets/javascripts/about_us_testimonial.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-	setTimeout(function() {
-    $(".test1").addClass("fadeIn");
-	}, 5000);
-    setTimeout(function() {
-    $(".test1").addClass("hidden");
-    $(".test2").removeClass("hidden");
-    }, 15000);
-    setTimeout(function() {
-    $(".test2").addClass("hidden");
-    $(".test3").removeClass("hidden");
-    }, 25000);
+// $(document).ready(function(){
+// 	setTimeout(function() {
+//     $(".test1").addClass("fadeIn");
+// 	}, 5000);
+//     setTimeout(function() {
+//     $(".test1").addClass("hidden");
+//     $(".test2").removeClass("hidden");
+//     }, 15000);
+//     setTimeout(function() {
+//     $(".test2").addClass("hidden");
+//     $(".test3").removeClass("hidden");
+//     }, 25000);
     
-})
+// })
